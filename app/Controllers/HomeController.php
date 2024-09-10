084c4e42f2eb99da8bc50e4f8d31819a183fbf79
Mostafa Zareei <mostafa.zareei.pub@gmail.com>
2024-09-10 10:46:56 +0330

    add HomeController to handle incoming request from the URI: /

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Kernel\Http\Controller;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        echo "Home Controller";
+    }
+}
