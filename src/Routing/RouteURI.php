7c802de752d7d3aeaaeccf3964662b62ffb0fdcb
Mostafa Zareei <mostafa.zareei.pub@gmail.com>
2024-09-10 10:29:27 +0330

    add RouteURI class for handling route information in the application

---
+++ src/Routing/RouteURI.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Kernel\Routing;
+
+use Closure;
+
+class RouteURI
+{
+    private string $uri;
+    private string $method;
+    private Closure | array | string $action;
+    private array $middlewares = [];
+
+    public function setUri(string $uri): static
+    {
+        $this->uri = $uri;
+        return $this;
+    }
+
+    public function setMethod(string $method): static
+    {
+        $this->method = $method;
+        return $this;
+    }
+
+    public function setAction(Closure | array | string $action): static
+    {
+        $this->action = $action;
+        return $this;
+    }
+
+    public function setMiddleware(mixed $middleware): static
+    {
+        $this->middlewares[] = $middleware;
+        return $this;
+    }
+
+    public function uri(): string
+    {
+        return $this->uri;
+    }
+
+    public function method(): string
+    {
+        return $this->method;
+    }
+
+    public function action(): Closure | array | string
+    {
+        return $this->action;
+    }
+
+    public function middlewares(): array
+    {
+        return $this->middlewares;
+    }
+}
