2772c2e9d73ee5c30d7487cf6f1f1b90d48bf97a
Mostafa Zareei <mostafa.zareei.pub@gmail.com>
2024-09-10 10:58:46 +0330

    initialize app and send request to router

--- public/index.php
+++ public/index.php
@@ -3,3 +3,9 @@
 define("ROOT_PATH", __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR);
 
 require ROOT_PATH . "vendor/autoload.php";
+
+use App\Kernel\Core\App;
+
+$app = new App();
+
+$app->handle();
