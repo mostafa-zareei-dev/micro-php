446b39cd2cf2b2a6085010c56c5b2bb365ae2d63
Mostafa Zareei <mostafa.zareei.pub@gmail.com>
2024-09-13 13:08:26 +0330

    bind route middlewares to the application container

--- src/ServiceProviders/AppServiceProvider.php
+++ src/ServiceProviders/AppServiceProvider.php
@@ -6,13 +6,22 @@ use App\Kernel\Container\ServiceContainer;
 use App\Kernel\Views\IView;
 use App\Kernel\Views\TwigView;
 
-class AppServiceProvider implements IServiceProvider {
+class AppServiceProvider implements IServiceProvider
+{
     public function register(ServiceContainer $container): void
     {
         $container->singleton(IView::class, TwigView::class);
+        $this->bindMiddlewares($container);
     }
 
-    public function boot(ServiceContainer $container): void {
-        
+    public function boot(ServiceContainer $container): void {}
+
+    private function bindMiddlewares(ServiceContainer $container)
+    {
+        $routeMiddlewares = configs('route_middlewares');
+
+        foreach ($routeMiddlewares as $middlewareKey => $middlewareClass) {
+            $container->singleton($middlewareKey, $middlewareClass);
+        }
     }
 }
