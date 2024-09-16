619ce7d809be48ede33a7c3564f259df2547e85f
Mostafa Zareei <mostafa.zareei.pub@gmail.com>
2024-09-16 12:17:55 +0330

    add loadEnvVariables method for loading app environment variables

--- src/Bootstraping/Bootstrap.php
+++ src/Bootstraping/Bootstrap.php
@@ -2,10 +2,22 @@
 
 namespace App\Kernel\Bootstraping;
 
+use Dotenv\Dotenv;
+
 class Bootstrap
 {
+    public static $envPath = ROOT_PATH;
+
     public static function loadFunctions(): void
     {
         include ROOT_PATH . "src/Utility/functions.php";
+    }
+
+    public static function loadEnvVariables(string $path = '')
+    {
+        $dotenv = Dotenv::createImmutable(
+            empty($path) ? self::$envPath : $path
+        );
+        $dotenv->load();
     }
 }
