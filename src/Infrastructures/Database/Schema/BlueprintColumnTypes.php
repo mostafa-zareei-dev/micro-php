d0eea72f5d0ed32dd0390b4c94b094bb7fa666c4
Mostafa Zareei <mostafa.zareei.pub@gmail.com>
2024-09-23 12:37:54 +0330

    add this class to handle column data types

---
+++ src/Infrastructures/Database/Schema/BlueprintColumnTypes.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Kernel\Infrastructures\Database\Schema;
+
+class BlueprintColumnTypes {
+    public const ID = "UNSIGNED BIGINT";
+    public const INT = "INT";
+    public const BIGINCREMENTS = "UNSIGNED BIGINT";
+    public const BIGINTEGER = "BIGINT";
+    public const BINARY = "BLOB";
+    public const BOOLEAN = "BOOLEAN";
+    public const CHAR = "CHAR";
+    public const DATE = "DATE";
+    public const DATETIME = "DATETIME";
+    public const STRING = "VARCHAR";
+    public const TEXT = "TEXT";
+    public const INTEGER = "INTEGER";
+    public const FOREIGNID = "UNSIGNED BIGINT";
+    public const JSON  = "JSON";
+}
